d1613b81f321430fd08bad7033d12a2e3cacf70a
AMJL-16 <>
2023-09-27 17:00:00 +0000

    added query 1-6 to sql-expression.py and created sql-orm.py file

--- sql-expression.py
+++ sql-expression.py
@@ -43,7 +43,22 @@ track_table = Table(
 with db.connect() as connection:
 
     # Query 1 - Select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Query 2 - Select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 - Select only 'Queen' from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 - Select only 'ArtistId' #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - Select only the albums with 'ArtistId' #51 on the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - Select all the tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
 
     results = connection.execute(select_query)
